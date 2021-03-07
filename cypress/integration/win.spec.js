dd178759082573d34e60688698e7eb9c61f09d59
natsume0718 <moto718program@gmail.com>
2021-03-07 23:09:01 +0900

    winne test

---
+++ cypress/integration/win.spec.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+describe('winner x', function() {
+  beforeEach(() => {
+    cy.visit(Cypress.env('app_host'));
+  });
+  it('Winner: X', () => {
+    cy.get('[data-cy=0]').click()
+    cy.get('[data-cy=1]').click()
+    cy.get('[data-cy=2]').click()
+    cy.get('[data-cy=3]').click()
+    cy.get('[data-cy=4]').click()
+    cy.get('[data-cy=5]').click()
+    cy.get('[data-cy=6]').click()
+  })
+})
